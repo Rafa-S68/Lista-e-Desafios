1e0457a0a7cc6cc0222fc33ad4dcf8504ee08bd6
Rafa-S68 <rafaela.srodrigues@aluno.faculdadeimpacta.com.br>
2025-03-12 06:05:04 -0300

    Update Desafio_09.kt

--- Desafio_09.kt
+++ Desafio_09.kt
@@ -1,17 +1,16 @@
 fun main() {
-    val vetor = IntArray(20) { it }  // Preenche o vetor com números de 0 a 19
+    val vetor = IntArray(20) { it }  
 
     println("Vetor com números:")
-    // Exibindo os números gerados
+  
     for (num in vetor) {
         println(num)
     }
 
-    // Ordenando o vetor em ordem crescente (neste caso já está em ordem)
     vetor.sort()
 
     println("\nVetor ordenado em ordem crescente:")
-    // Exibindo os números ordenados
+    
     for (num in vetor) {
         println(num)
     }
