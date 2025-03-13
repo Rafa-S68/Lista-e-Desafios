8bd9dabffb27b482c3fba86f59f70e91ceee7e84
Rafa-S68 <rafaela.srodrigues@aluno.faculdadeimpacta.com.br>
2025-03-12 23:38:37 -0300

    Update1 Desafio_02.kt
    Versão atualizada

--- Desafio_02.kt
+++ Desafio_02.kt
@@ -1,18 +1,24 @@
 fun main() {
     print("Digite o comprimento do primeiro lado: ")
-    val lado1 = readLine()?.toDoubleOrNull() ?: return
+    val lado1 = readLine()!!.toFloat()
 
     print("Digite o comprimento do segundo lado: ")
-    val lado2 = readLine()?.toDoubleOrNull() ?: return
+    val lado2 = readLine()!!.toFloat()
 
     print("Digite o comprimento do terceiro lado: ")
-    val lado3 = readLine()?.toDoubleOrNull() ?: return
+    val lado3 = readLine()!!.toFloat()
 
     if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) {
         val tipoTriangulo = when {
-            lado1 == lado2 && lado2 == lado3 -> "EQUILÁTERO"
-            lado1 == lado2 || lado1 == lado3 || lado2 == lado3 -> "ISÓSCELES"
-            else -> "ESCALENO"
+            lado1 == lado2 && lado2 == lado3 -> {
+                "EQUILÁTERO"
+            }
+            lado1 == lado2 || lado1 == lado3 || lado2 == lado3 -> {
+                "ISÓSCELES"
+            }
+            else -> {
+                "ESCALENO"
+            }
         }
         println("Os lados informados formam um triângulo $tipoTriangulo.")
     } else {
